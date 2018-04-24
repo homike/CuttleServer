eec411e819956d2a88e95786dcb98309ebac04e1
moyu <403231700@qq.com>
2018-04-24 17:54:34 +0800

    gate修改

--- gateserver/network/acceptor.go
+++ gateserver/network/acceptor.go
@@ -13,7 +13,32 @@ func (self *Acceptor) Start(addr string) error {
 
 	ln, err := net.Listen("TCP", addr)
 	if err != nil {
-		fmt.Printf("Acceptor Start(%v) error (%v) \n", addr, err)
+		fmt.Printf("Acceptor Start(%v) error: %v \n", addr, err)
+		return err
+	}
+
+	self.Listener = ln
+
+	go self.Accept()
+}
+
+func (self *Acceptor) Accept() error {
+	for {
+		conn, err := self.Listener.Accept()
+		if err != nil {
+			fmt.Printf("Accepter Accept() error: %v \n ", addr, err)
+			return err
+		}
+
+		go self.OnAccept(conn)
+	}
+}
+
+func (self *Acceptor) OnAccept(conn net.Conn) error {
+
+	sess, err := NewSession(conn)
+	if err != nil {
+		return err
 	}
 
 }
--- gateserver/network/session.go
+++ gateserver/network/session.go
@@ -4,6 +4,8 @@ import "net"
 
 type session struct {
 	Conn net.Conn
+
+	WriteChann chan []byte
 }
 
 func NewSession(conn net.Conn) (session, error) {
@@ -11,4 +13,22 @@ func NewSession(conn net.Conn) (session, error) {
 		Conn: conn,
 	}
 	return sess, nil
+}
+
+func (self *session) Run() {
+
+	go sendLoop()
+}
+
+func (self *session) sendLoop() {
+	for b := range WriteChann {
+		if b == nil {
+			break
+		}
+
+		_, err := self.Conn.Write(b)
+		if err != nil {
+			break
+		}
+	}
 }
